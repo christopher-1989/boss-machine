02204e1674deada8c05765136577911d76f7c7f3
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-07-24 08:23:28 +1000

    only meeting router to go

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,12 +1,13 @@
-const checkMillionDollarIdea = (res, req, next) => {
-    const { numWeeks, weeklyRevenue } = req.body
-    const totalYield = Number(numWeeks) * Number(weeklyRevenue)
-    if (totalYield < 1000000) {
-        res.status(400).send()
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body;
+    const totalMoney = Number(numWeeks) * Number(weeklyRevenue);
+    if (!numWeeks || !weeklyRevenue || isNaN(totalMoney) || totalMoney < 1000000) {
+      res.status(400).send();
     } else {
-        next()
+      next();
     }
-};
-
-// Leave this exports assignment so that the function can be used elsewhere
-module.exports = checkMillionDollarIdea;
+  }
+  
+  // Leave this exports assignment so that the function can be used elsewhere
+  module.exports = checkMillionDollarIdea;
+  
